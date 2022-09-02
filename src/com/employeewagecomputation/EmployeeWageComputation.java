0a23560eda8b957f4dce14117e11dbc45fd8c1c8
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-02 13:21:41 +0530

    ADDED: employee daily wage

--- src/com/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/employeewagecomputation/EmployeeWageComputation.java
@@ -2,13 +2,17 @@ package com.employeewagecomputation;
 
 public class EmployeeWageComputation {
 	public final static int IS_PRESENT = 1;
+	public static double WAGE_PER_HOUR = 20;
+	public static double FULL_DAY_HOUR = 8;
 	
 	public static int attendance() {
 		if ((int) Math.floor(Math.random() * 10) % 2 == IS_PRESENT) {
 			System.out.println("Emplyee is present.");
+			System.out.println("Employee daily wage is "+(FULL_DAY_HOUR * WAGE_PER_HOUR));
 			return IS_PRESENT;
 		} else {
 			System.out.println("Employee is absenet.");
+			System.out.println("Employee daily wage is 0");
 			return 0;
 		}
 	}
